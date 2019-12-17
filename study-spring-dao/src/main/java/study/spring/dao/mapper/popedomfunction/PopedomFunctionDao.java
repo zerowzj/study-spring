ed29647316ec8e8dc866197f8ba469f2bcb0832b
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-17 14:34:13 +0800

    提交代码

--- study-spring-dao/src/main/java/study/spring/dao/mapper/popedomfunction/PopedomFunctionDao.java
+++ study-spring-dao/src/main/java/study/spring/dao/mapper/popedomfunction/PopedomFunctionDao.java
@@ -1,9 +1,7 @@
 package study.spring.dao.mapper.popedomfunction;
 
-import org.springframework.stereotype.Repository;
 import study.spring.dao.support.db.BaseDao;
 
-@Repository
 public interface PopedomFunctionDao extends BaseDao<Long, PopedomFunctionEO> {
 
 }
--- study-spring-dao/src/main/java/study/spring/dao/mapper/popedomrole/PopedomRoleDao.java
+++ study-spring-dao/src/main/java/study/spring/dao/mapper/popedomrole/PopedomRoleDao.java
@@ -1,9 +1,7 @@
 package study.spring.dao.mapper.popedomrole;
 
-import org.springframework.stereotype.Repository;
 import study.spring.dao.support.db.BaseDao;
 
-@Repository
 public interface PopedomRoleDao extends BaseDao<Long, PopedomRoleEO> {
 
 }
