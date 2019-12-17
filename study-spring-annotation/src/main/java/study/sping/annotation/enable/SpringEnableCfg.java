3dc1f5a82c1ad47825235e4b863f36f67609d551
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-12-17 17:24:09 +0800

    更新代码

---
+++ study-spring-annotation/src/main/java/study/sping/annotation/enable/SpringEnableCfg.java
@@ -0,0 +1,12 @@
+package study.sping.annotation.enable;
+
+import org.springframework.context.annotation.Configuration;
+import study.sping.annotation.enable.hello.EnableHello;
+import study.sping.annotation.enable.server.EnableServer;
+import study.sping.annotation.enable.server.Server;
+
+@EnableServer(type = Server.Type.FTP)
+@EnableHello
+@Configuration
+public class SpringEnableCfg {
+}
