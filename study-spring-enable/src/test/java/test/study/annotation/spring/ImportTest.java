b7e7fa971175a7290cb616a07769923f0fd97701
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-08-19 14:41:05 +0800

    更新代码

--- study-spring-enable/src/test/java/test/study/annotation/spring/ImportTest.java
+++ study-spring-enable/src/test/java/test/study/annotation/spring/ImportTest.java
@@ -1,12 +1,10 @@
 package test.study.annotation.spring;
 
 import org.junit.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import study.spring.enable.impot.Cstm_Bean;
 import study.spring.enable.impot.ImportCfg;
 
-@EnableAutoConfiguration
 public class ImportTest {
 
     @Test
