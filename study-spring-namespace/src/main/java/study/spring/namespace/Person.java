eb759dcbe0a09b9cc84ea6fb0bfa60b6259bb928
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-08-20 16:56:19 +0800

    更新代码

--- study-spring-namespace/src/main/java/study/spring/namespace/Person.java
+++ study-spring-namespace/src/main/java/study/spring/namespace/Person.java
@@ -3,15 +3,23 @@ package study.spring.namespace;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 
+@Slf4j
 @Setter
 @Getter
 @ToString
-public class Person {
+public class Person implements InitializingBean {
 
     private String id;
 
     private String name;
 
     private int age;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.info("safdfasdfasdf");
+    }
 }
