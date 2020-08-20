83183806b71431fe7f0f52d91a544ee72deb4c2d
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-08-20 16:52:34 +0800

    更新代码

--- study-spring-namespace/src/main/java/study/spring/namespace/handler/PersonNamespaceHandler.java
+++ study-spring-namespace/src/main/java/study/spring/namespace/handler/PersonNamespaceHandler.java
@@ -1,12 +1,13 @@
 package study.spring.namespace.handler;
 
 import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+import study.spring.namespace.parser.DemoBeanDefinitionParser;
 import study.spring.namespace.parser.PersonBeanDefinitionParser;
 
 public class PersonNamespaceHandler extends NamespaceHandlerSupport {
 
     @Override
     public void init() {
-        registerBeanDefinitionParser("person", new PersonBeanDefinitionParser());
+        registerBeanDefinitionParser("person", new DemoBeanDefinitionParser());
     }
 }
--- study-spring-namespace/src/main/java/study/spring/namespace/parser/DemoBeanDefinitionParser.java
+++ study-spring-namespace/src/main/java/study/spring/namespace/parser/DemoBeanDefinitionParser.java
@@ -1,20 +1,35 @@
 package study.spring.namespace.parser;
 
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.xml.BeanDefinitionParser;
 import org.springframework.beans.factory.xml.ParserContext;
-import org.springframework.util.StringUtils;
+import org.springframework.beans.factory.xml.XmlReaderContext;
 import org.w3c.dom.Element;
+import study.spring.namespace.Person;
 
 public class DemoBeanDefinitionParser implements BeanDefinitionParser {
 
     @Override
-    public BeanDefinition parse(Element element, ParserContext parserContext) {
+    public BeanDefinition parse(Element element, ParserContext context) {
+        //
+        Object source = context.extractSource(element);
+        //
+        XmlReaderContext readerContext = context.getReaderContext();
+        //定义Bean
+        RootBeanDefinition handlerMappingDef = new RootBeanDefinition(Person.class);
+        handlerMappingDef.setSource(source);
+        handlerMappingDef.setRole(BeanDefinition.ROLE_INFRASTRUCTURE);
+        handlerMappingDef.getPropertyValues().add("id", "cutesource");
+        handlerMappingDef.getPropertyValues().add("name", "fdasfads");
+        handlerMappingDef.getPropertyValues().add("age", "100");
+        //
+        BeanDefinitionRegistry registry = readerContext.getRegistry();
+        registry.registerBeanDefinition("cutesource", handlerMappingDef);
 
-        String id = element.getAttribute("id");
-        if (StringUtils.hasText(id)) {
-
-        }
+//        context.registerComponent(new BeanComponentDefinition(handlerMappingDef, ""));
+//        context.popAndRegisterContainingComponent();
         return null;
     }
 }
