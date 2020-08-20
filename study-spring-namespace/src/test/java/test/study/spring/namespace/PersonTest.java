c5488f508cd414ae5d5ca42120bf60d215426b10
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-08-20 17:55:53 +0800

    更新代码

--- study-spring-namespace/src/test/java/test/study/spring/namespace/PersonTest.java
+++ study-spring-namespace/src/test/java/test/study/spring/namespace/PersonTest.java
@@ -2,11 +2,16 @@ package test.study.spring.namespace;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import study.spring.namespace.Person;
 
 @Slf4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/spring-context.xml")
 public class PersonTest {
 
     @Test
