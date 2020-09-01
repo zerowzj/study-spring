653e6df80d0071bde92d5619217e8745de918b6e
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-09-01 16:24:28 +0800

    更新代码

--- study-spring-ioc_aop/src/test/java/test/study/spring/ioc/LifecycleTest.java
+++ study-spring-ioc_aop/src/test/java/test/study/spring/ioc/LifecycleTest.java
@@ -10,7 +10,7 @@ import study.spring.ioc.bean.LifecycleBean;
 public class LifecycleTest {
 
     @Test
-    public void test() throws Exception {
+    public void config_test() {
         String configLocation = "spring/spring-lifecycle.xml";
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(configLocation);
         //不显示close不执行destroy-method方法
@@ -18,7 +18,7 @@ public class LifecycleTest {
     }
 
     @Test
-    public void test2(){
+    public void annot_test() {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
         ctx.registerBean(LifecycleBean.class);
         //需要手动刷新
