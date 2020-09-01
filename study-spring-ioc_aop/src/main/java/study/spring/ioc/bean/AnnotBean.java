cf42594a71363cb3ba58054c0824d66b47ee1381
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-09-01 16:23:28 +0800

    更新代码

--- study-spring-ioc_aop/src/main/java/study/spring/ioc/bean/AnnotBean.java
+++ study-spring-ioc_aop/src/main/java/study/spring/ioc/bean/AnnotBean.java
@@ -1,8 +1,11 @@
 package study.spring.ioc.bean;
 
+import com.sun.org.apache.xml.internal.security.Init;
 import lombok.Getter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -12,30 +15,53 @@ import javax.annotation.PreDestroy;
 @Getter
 @ToString
 @Component
-public class AnnotBean {
+public class AnnotBean implements InitializingBean, DisposableBean {
 
     private String name;
 
+    //********** 初始化和实例化 **********
     static {
-        log.info(">>>>>> 初始化");
+        log.info("====== 初始化");
     }
 
     public AnnotBean() {
-        log.info(">>>>>> 实例化");
+        log.info("====== 实例化");
     }
 
+    //********** setter依赖注入 **********
+    public void setName(String name) {
+        log.info("====== set name value");
+        this.name = name;
+    }
+
+    //********** 注解 **********
     @PostConstruct
     public void init() {
-        log.info(">>>>>> @PostConstruct");
+        log.info("====== @PostConstruct");
     }
 
     @PreDestroy
-    public void destroy() {
-        log.info(">>>>>> @PreDestroy");
+    public void preDestroy() {
+        log.info("====== @PreDestroy");
     }
 
-    public void setName(String name) {
-        log.info(">>>>>> set name value");
-        this.name = name;
+    //********** InitializingBean和DisposableBean **********
+    @Override
+    public void destroy() throws Exception {
+        log.info("====== destroy()");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.info("====== afterPropertiesSet()");
+    }
+
+    //********** init-method和destroy-method **********
+    public void myInit() {
+        log.info("====== myInit()");
+    }
+
+    public void myDestroy() {
+        log.info("====== myDestroy()");
     }
 }
--- study-spring-ioc_aop/src/main/java/study/spring/ioc/bean/ConfigBean.java
+++ study-spring-ioc_aop/src/main/java/study/spring/ioc/bean/ConfigBean.java
@@ -3,35 +3,62 @@ package study.spring.ioc.bean;
 import lombok.Getter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Slf4j
 @Getter
 @ToString
-public class ConfigBean {
+public class ConfigBean implements InitializingBean, DisposableBean {
 
     private String name;
 
+    //********** 初始化和实例化 **********
     static {
-        log.info(">>>>>> 初始化");
+        log.info("====== 初始化");
     }
 
     public ConfigBean() {
-        log.info(">>>>>> 实例化");
+        log.info("====== 实例化");
     }
 
+    //********** setter依赖注入 **********
     public void setName(String name) {
-        log.info(">>>>>> set name value");
+        log.info("====== set name value");
         this.name = name;
     }
 
+    //********** 注解 **********
+    @PostConstruct
     public void init() {
-        log.info(">>>>>> init");
+        log.info("====== @PostConstruct");
     }
 
-    public void destroy() {
-        log.info(">>>>>> destroy");
+    @PreDestroy
+    public void preDestroy() {
+        log.info("====== @PreDestroy");
+    }
+
+    //********** InitializingBean和DisposableBean **********
+    @Override
+    public void destroy() throws Exception {
+        log.info("====== destroy()");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.info("====== afterPropertiesSet()");
+    }
+
+    //********** init-method和destroy-method **********
+    public void myInit() {
+        log.info("====== myInit()");
+    }
+
+    public void myDestroy() {
+        log.info("====== myDestroy()");
     }
 }
