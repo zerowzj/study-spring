1aa447124a1178990d1e9a754e00b9778450168a
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-09-03 16:35:50 +0800

    更新代码

--- study-spring-aop/src/test/java/test/study/spring/aop/ProxyFactoryTest.java
+++ study-spring-aop/src/test/java/test/study/spring/aop/ProxyFactoryTest.java
@@ -5,21 +5,26 @@ import org.junit.Test;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.NameMatchMethodPointcutAdvisor;
 import study.spring.aop.GreetingService;
+import study.spring.aop.advice.MyAfterReturningAdvice;
 import study.spring.aop.advice.MyBeforeAdvice;
 
 @Slf4j
 public class ProxyFactoryTest {
 
     @Test
-    public void test(){
-        NameMatchMethodPointcutAdvisor advisor = new NameMatchMethodPointcutAdvisor();
-        advisor.setMappedName("sayHi");
-        advisor.setAdvice(new MyBeforeAdvice());
+    public void test() {
+        NameMatchMethodPointcutAdvisor advisor1 = new NameMatchMethodPointcutAdvisor();
+        advisor1.setMappedName("sayHi");
+        advisor1.setAdvice(new MyBeforeAdvice());
+        NameMatchMethodPointcutAdvisor advisor2 = new NameMatchMethodPointcutAdvisor();
+        advisor2.setMappedName("sayHi");
+        advisor2.setAdvice(new MyAfterReturningAdvice());
 
         ProxyFactory proxyFactory = new ProxyFactory();
         proxyFactory.setTarget(new GreetingService());
-        proxyFactory.addAdvisor(advisor);
-        GreetingService greetingService = (GreetingService)proxyFactory.getProxy();
-        greetingService.sayHi("fsadfs");
+        proxyFactory.addAdvisor(advisor1);
+        proxyFactory.addAdvisor(advisor2);
+        GreetingService greetingService = (GreetingService) proxyFactory.getProxy();
+        greetingService.sayHi("wangzhj");
     }
 }
