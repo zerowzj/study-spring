d8f878fca3505d6714e50b54d9a0c9e6f9ca84b9
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-11-17 15:40:10 +0800

    更新代码

--- study-spring-ioc/src/main/java/study/spring/ioc/bean_factory/xml/XmlBean.java
+++ study-spring-ioc/src/main/java/study/spring/ioc/bean_factory/xml/XmlBean.java
@@ -5,7 +5,6 @@ import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -19,48 +18,48 @@ public class XmlBean implements InitializingBean, DisposableBean {
 
     //初始化和实例化
     static {
-        log.info("====== 初始化 ======");
+        log.info("====== 初始化");
     }
 
     public XmlBean() {
-        log.info("====== 实例化 ======");
+        log.info("====== 实例化");
     }
 
     //setter依赖注入
     public void setName(String name) {
-        log.info("====== set name value ======");
+        log.info("====== set name value");
         this.name = name;
     }
 
     //注解
     @PostConstruct
     public void init() {
-        log.info("====== @PostConstruct ======");
+        log.info("====== @PostConstruct");
     }
 
     @PreDestroy
     public void preDestroy() {
-        log.info("====== @PreDestroy ======");
+        log.info("====== @PreDestroy");
     }
 
     //InitializingBean和DisposableBean
     @Override
     public void destroy() throws Exception {
-        log.info("====== destroy ======");
+        log.info("====== destroy");
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        log.info("====== afterPropertiesSet ======");
+        log.info("====== afterPropertiesSet");
     }
 
     //nit-method和destroy-method
     public void myInit(){
-        log.info("====== myInit ======");
+        log.info("====== myInit");
     }
 
     public void myDestroy(){
-        log.info("====== myDestroy ======");
+        log.info("====== myDestroy");
     }
 
 }
