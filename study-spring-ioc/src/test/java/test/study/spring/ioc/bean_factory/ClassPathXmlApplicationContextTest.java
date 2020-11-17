d7f6d9d3cc395f4d5eacfa5322a5ea62b4328701
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-11-17 15:26:17 +0800

    更新代码

---
+++ study-spring-ioc/src/test/java/test/study/spring/ioc/bean_factory/ClassPathXmlApplicationContextTest.java
@@ -0,0 +1,18 @@
+package test.study.spring.ioc.bean_factory;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@Slf4j
+public class ClassPathXmlApplicationContextTest {
+
+    String CONFIG_LOCATION = "spring/spring-xml.xml";
+
+    @Test
+    public void test() {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+        log.info(">>>>>> ");
+        ctx.getBean("xmlBean");
+    }
+}
---
+++ study-spring-ioc/src/test/java/test/study/spring/ioc/bean_factory/XmlBeanFactoryTest.java
@@ -0,0 +1,22 @@
+package test.study.spring.ioc.bean_factory;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+@Slf4j
+public class XmlBeanFactoryTest {
+
+    String CONFIG_LOCATION = "spring/spring-xml.xml";
+
+    @Test
+    public void test() {
+        Resource resource = new ClassPathResource(CONFIG_LOCATION);
+        BeanFactory factory = new XmlBeanFactory(resource);
+        log.info(">>>>>>");
+        factory.getBean("xmlBean");
+    }
+}
