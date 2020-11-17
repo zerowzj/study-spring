e3f5ba88877c907bd47634b0e4a6bbd9d43680f0
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-11-17 16:02:30 +0800

    更新代码

--- study-spring-ioc/src/test/java/test/study/spring/ioc/bean_factory/XmlTest.java
+++ study-spring-ioc/src/test/java/test/study/spring/ioc/bean_factory/XmlTest.java
@@ -5,19 +5,15 @@ import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import study.spring.ioc.bean_factory.xml.XmlBean;
 
-import java.util.concurrent.TimeUnit;
-
 @Slf4j
 public class XmlTest {
 
     private static final String CONFIG_LOCATION = "spring/spring-xml.xml";
 
     @Test
-    public void test() throws Exception {
+    public void test() {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
-        log.info("容器启动完成");
-        //
-        TimeUnit.SECONDS.sleep(5);
+
         XmlBean xmlBean = (XmlBean) ctx.getBean("xmlBean");
         log.info("{}", xmlBean);
 
