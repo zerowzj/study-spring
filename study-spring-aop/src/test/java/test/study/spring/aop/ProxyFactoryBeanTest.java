856ea320acc888ad0e3b8a91949cbba2e05b4630
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-11-18 14:05:05 +0800

    更新代码

---
+++ study-spring-aop/src/test/java/test/study/spring/aop/ProxyFactoryBeanTest.java
@@ -0,0 +1,33 @@
+package test.study.spring.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import study.spring.aop.GreetingService;
+import study.spring.aop.xml.advice.CustomBeforeAdvice;
+
+@Slf4j
+public class ProxyFactoryBeanTest {
+
+    @Test
+    public void test() {
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        //
+        BeanDefinitionBuilder b1 = BeanDefinitionBuilder.genericBeanDefinition(CustomBeforeAdvice.class);
+        BeanDefinition bd1 = b1.getBeanDefinition();
+        factory.registerBeanDefinition("customBeforeAdvice", bd1);
+        //
+        BeanDefinitionBuilder b2 = BeanDefinitionBuilder.genericBeanDefinition(ProxyFactoryBean.class);
+        b2.addPropertyValue("target", new GreetingService());
+        b2.addPropertyValue("interceptorNames", "customBeforeAdvice");
+        BeanDefinition bd2 = b2.getBeanDefinition();
+        factory.registerBeanDefinition("greetingService", bd2);
+
+        GreetingService greetingService = (GreetingService) factory.getBean("greetingService");
+        greetingService.sayHi("11111111111");
+
+    }
+}
