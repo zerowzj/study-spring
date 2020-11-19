9cea1a8bbbaa8e4ee43cbdf2b2686a124d1412f6
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-11-19 18:06:12 +0800

    更新代码

--- study-spring-tx/src/test/java/test/study/spring/tx/TxBeanTest.java
+++ study-spring-tx/src/test/java/test/study/spring/tx/TxBeanTest.java
@@ -2,12 +2,15 @@ package test.study.spring.tx;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @Slf4j
 public class TxBeanTest {
 
+    String CONFIG_LOCATION = "spring/spring-tx-bean.xml";
+
     @Test
     public void test() {
-
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
     }
 }
--- study-spring-tx/src/test/java/test/study/spring/tx/TxSchemaTest.java
+++ study-spring-tx/src/test/java/test/study/spring/tx/TxSchemaTest.java
@@ -2,12 +2,15 @@ package test.study.spring.tx;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @Slf4j
 public class TxSchemaTest {
 
-    @Test
-    public void test(){
+    String CONFIG_LOCATION = "spring/spring-tx-schema.xml";
 
+    @Test
+    public void test() {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
     }
 }
